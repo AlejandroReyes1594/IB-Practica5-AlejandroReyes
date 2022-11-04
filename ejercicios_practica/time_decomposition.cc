167f43323520d8c0d11df5396509bfe575714781
alu0101552316@ull.edu.es <alu0101552316@ull.edu.es>
2022-11-04 18:35:33 +0000

    first commit

---
+++ ejercicios_practica/time_decomposition.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	int tiempo = 3600;
+	int tiempo2 = 60;
+	cin>> n ;
+	cout<< n/tiempo<<"h"<< "+" << n/tiempo2 <<"m"<< "+" << n <<"s"<<"=n"<< endl;
+}
